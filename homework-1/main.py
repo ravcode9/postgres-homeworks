4dc8b15d98da756ac85ecb7f73ebe1ef33df2dad
ravcode9 <rlatypov2106@gmail.com>
2024-02-01 17:09:36 +0500

    1 homework

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,43 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import os
+import psycopg2 as ps
+import csv
+
+
+def load_data_from_csv(file_name, table_name):
+    file_path = os.path.join(os.path.dirname(__file__), 'north_data', file_name)
+
+    conn = None
+    cur = None
+
+    try:
+        conn = ps.connect(host='localhost', database='north', user='postgres', password='12345')
+        cur = conn.cursor()
+
+        if os.path.isfile(file_path):
+            with open(file_path, 'r', encoding='UTF-8') as file:
+                csv_reader = csv.reader(file)
+                next(csv_reader)
+
+                for row in csv_reader:
+                    cur.execute(
+                        f"INSERT INTO {table_name} VALUES ({','.join(['%s'] * len(row))})", row
+                    )
+
+            conn.commit()
+            print(f"Данные успешно загружены в таблицу {table_name} из файла {file_path}.")
+        else:
+            print(f"Файл '{file_path}' не существует.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+
+load_data_from_csv('employees_data.csv', 'employees')
+load_data_from_csv('customers_data.csv', 'customers')
+load_data_from_csv('orders_data.csv', 'orders')
+
+
