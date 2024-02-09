78eb158c4c02440a3e58fbbbc975ae24af1bb1d0
ravcode9 <rlatypov2106@gmail.com>
2024-02-09 14:07:15 +0500

    2 homework

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -14,8 +14,10 @@ WHERE discontinued = 1
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
-DELETE FROM order_details
-WHERE product_id IN (SELECT product_id FROM products WHERE discontinued = 1)
-
-DELETE FROM products
-WHERE discontinued = 1
+ALTER TABLE order_details DROP CONSTRAINT fk_order_details_products;
+ALTER TABLE order_details ADD CONSTRAINT fk_order_details_products
+  FOREIGN KEY (product_id)
+  REFERENCES products(product_id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+DELETE FROM products WHERE discontinued = 1
